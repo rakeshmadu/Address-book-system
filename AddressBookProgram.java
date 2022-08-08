060bb7a5e800a4888d865a2ab4feba037823ba78
Madu Mahesh Kumar <rakeshmadu24@gmail.com>
2022-08-08 18:11:02 +0530

    welcome

---
+++ AddressBookProgram.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.AddressBook;
+
+public class AddressBookProgram {
+
+    public static void main(String[] args){
+
+        System.out.println("Welcome To Address Book Program.");
+    }
+}
