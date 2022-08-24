81b7b4c2f071bf1dcbfef75fb41ceda235026b31
Madu Mahesh Kumar <rakeshmadu24@gmail.com>
2022-08-24 19:10:36 +0530

    uc9

---
+++ src/com/address/entity/AddressBookRegistry.java
@@ -0,0 +1,38 @@
+package com.address.entity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class AddressBookRegistry {
+
+	public static HashMap<String, ArrayList<ContactPerson>> CityPersonMap = new HashMap<>();
+	public static HashMap<String, ArrayList<ContactPerson>> StatePersonMap = new HashMap<>();
+
+	public static HashMap<String, ArrayList<ContactPerson>> getCityPersonMap() {
+		return CityPersonMap;
+	}
+
+	public static void setCityPersonMap(HashMap<String, ArrayList<ContactPerson>> cityPersonMap) {
+		CityPersonMap = cityPersonMap;
+	}
+
+	public static HashMap<String, ArrayList<ContactPerson>> getStatePersonMap() {
+		return StatePersonMap;
+	}
+
+	public static void setStatePersonMap(HashMap<String, ArrayList<ContactPerson>> statePersonMap) {
+		StatePersonMap = statePersonMap;
+	}
+
+	private static HashSet<AddressBook> AddressBookList = new HashSet<>();
+
+	public static HashSet<AddressBook> getAddressBookList() {
+		return AddressBookList;
+	}
+
+	public static void setAddressBookList(HashSet<AddressBook> addressBookList) {
+		AddressBookList = addressBookList;
+	}
+
+}
